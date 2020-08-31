d8bd7acc0388d465db103bf531dd60b6080048c9
unknown <rootrieqyns13@gmail.com>
2020-08-31 15:10:59 +0700

    add intersection min-height

--- src/pages/StudentResultListPage.vue
+++ src/pages/StudentResultListPage.vue
@@ -1,94 +1,99 @@
 <template>
-  <div>
-    <q-header
-      elevated
-    >
-      <q-toolbar class="bg-blue">
-        <q-icon name="poll" style="font-size:1.5em" />
-        <q-toolbar-title>
-          <div class="text-body1">Hasil</div>
-        </q-toolbar-title>
-        <q-space />
-      </q-toolbar>
+<div>
+    <q-header elevated>
+        <q-toolbar class="bg-blue">
+            <q-icon name="poll" style="font-size:1.5em" />
+            <q-toolbar-title>
+                <div class="text-body1">Hasil</div>
+            </q-toolbar-title>
+            <q-space />
+        </q-toolbar>
     </q-header>
 
-  <q-pull-to-refresh @refresh="init">
-    <div v-if="!StudentResult.items.data && !loading">
-      <div class="row">
-        <div class="col-12 q-pa-lg text-center">
-          <div class="text-h5 text-black">
-            Assigment masih kosong
-          </div>
+    <q-pull-to-refresh @refresh="init">
+        <div v-if="!StudentResult.items.data && !loading">
+            <div class="row">
+                <div class="col-12 q-pa-lg text-center">
+                    <div class="text-h5 text-black">
+                        Assigment masih kosong
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <q-infinite-scroll @load="onLoad" :offset="250" v-if="StudentResult.items.data">
-    <q-intersection class="q-pa-xs" v-for="(assigment, id) in StudentResult.items.data" :key="id">
-     <student-result-item-component :assigment="assigment"></student-result-item-component>
-    </q-intersection>
-    <template v-slot:loading>
-        <div class="row justify-center q-my-md">
-          <q-spinner-dots color="primary" size="40px" />
-        </div>
-      </template>
-    </q-infinite-scroll>
-  </q-pull-to-refresh>
-  </div>
+        <q-infinite-scroll @load="onLoad" :offset="250" v-if="StudentResult.items.data">
+            <q-intersection style="min-height:25vh;min-width:100vw" class="q-pa-xs" v-for="(assigment, id) in StudentResult.items.data" :key="id">
+                <student-result-item-component :assigment="assigment"></student-result-item-component>
+            </q-intersection>
+            <template v-slot:loading>
+                <div class="row justify-center q-my-md">
+                    <q-spinner-dots color="primary" size="40px" />
+                </div>
+            </template>
+        </q-infinite-scroll>
+    </q-pull-to-refresh>
+</div>
 </template>
 
 <script>
-import { mapGetters, mapState } from "vuex";
-import { Notify } from 'quasar'
-import { copyToClipboard } from 'quasar'
+import {
+    mapGetters,
+    mapState
+} from "vuex";
+import {
+    Notify
+} from 'quasar'
+import {
+    copyToClipboard
+} from 'quasar'
 
 export default {
-   components: {
-    StudentResultItemComponent: () =>
-      import("components/assigment/studentresult/ItemComponent.vue"),
-  },
-  data(){
-    return {
-      assigments:{
-        data:[]
-      },
-      tab: 'teacher',
-      loading:false,
-    }
-  },
-  computed:{
-    ...mapState(['Setting','Assigment','StudentResult'])
-  },
-  created(){
-     if (!this.StudentResult.items.data)
-      this.$store.dispatch("StudentResult/index");
-      
-  },
-  mounted(){
-   
-  },
-  methods:{
-    init(done) {
-      Promise.all([
-        this.$store.dispatch("Auth/getAuth"),
-        this.$store.dispatch("StudentResult/index"),
-      ]).then(res => {
-        if (done) done();
-      });
+    components: {
+        StudentResultItemComponent: () =>
+            import("components/assigment/studentresult/ItemComponent.vue"),
     },
-    onLoad(index, done){
-        this.StudentResult.items.next_page_url
-        ? this.$store.dispatch("StudentResult/next").then(res => done())
-        : done();
+    data() {
+        return {
+            assigments: {
+                data: []
+            },
+            tab: 'teacher',
+            loading: false,
+        }
+    },
+    computed: {
+        ...mapState(['Setting', 'Assigment', 'StudentResult'])
+    },
+    created() {
+        if (!this.StudentResult.items.data)
+            this.$store.dispatch("StudentResult/index");
+
+    },
+    mounted() {
+
+    },
+    methods: {
+        init(done) {
+            Promise.all([
+                this.$store.dispatch("Auth/getAuth"),
+                this.$store.dispatch("StudentResult/index"),
+            ]).then(res => {
+                if (done) done();
+            });
+        },
+        onLoad(index, done) {
+            this.StudentResult.items.next_page_url ?
+                this.$store.dispatch("StudentResult/next").then(res => done()) :
+                done();
+        }
     }
-  }
 };
 </script>
 
 <style>
 #rcorners1 {
-  border-radius: 8px;
-  background: #bae7e7;
-  padding-left: 10px;
-  width: 150px;
+    border-radius: 8px;
+    background: #bae7e7;
+    padding-left: 10px;
+    width: 150px;
 }
 </style>
