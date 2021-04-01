f78fc84ef55fb2320f88e4fda63aa66988c209bc
Zulham Azwar Achmad <zulham724@students.unnes.ac.id>
2021-04-01 15:53:12 +0700

    update mengganti metode pembayaran menggunakan api cek mutasi

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,87 +1,93 @@
-import Vue from "vue";
+// import Vue from "vue";
 import axios from "axios";
-import store from "./../store";
+// import store from "./../store";
 import moment from "moment";
 import { Notify } from "quasar";
 
-axios.interceptors.response.use(function (config) {
-    // Do something before request is sent
-    return config;
-  }, function (error) {
-    // Do something with request error
-    if(error.response.status==401){
-        store().commit("Auth/setUnauthorized",true);
-        store().dispatch("Auth/logout").then(res=>{
-            Notify.create('Silahkan login kembali');
-            
-
-        })
-        // console.log(router().currentRoute);
-      
-       
-    }
-    return Promise.reject(error);
-  });
-  
-Vue.prototype.$axios = axios;
-Vue.prototype.$http = axios;
-const token = store().getters["Auth/token"];
-if (token.access_token) {
-    Vue.prototype.$http.defaults.headers.common.Accept = "application/json";
-    Vue.prototype.$http.defaults.headers.common.Authorization = `${token.token_type} ${token.access_token}`;
-    // init
-    store()
-        .dispatch("Auth/getAuth")
-        .then(res => {
-            if (
-                res.data.user_activated_at != null &&
-                moment(new Date()).diff(
-                    new Date(res.data.user_activated_at),
-                    "months",
-                    true
-                ) > 6
-            ) {
-                Notify.create({
-                    position: "center",
-                    message: "Masa Penggunaan aplikasi  RPP DIGITAL, PENILAIAN DIGITAL, dan MODUL DIGITAL sudah habis.  Silakan melakukan pembayaran iuran untuk menambah masa pemakaian.",
-                    textColor: "white",
-                    actions: [{
-                            label: "Konfirmasi",
-                            color: "white",
-                            handler: () => {
-                                store()
-                                    .dispatch("Auth/getPaymentStatus")
-                                    .then(res => {
-                                        if (moment(new Date()).diff(
-                                                new Date(res.data.user_activated_at),
-                                                "months",
-                                                true
-                                            ) <= 6) {
-                                            Notify.create({
-                                                position: "center",
-                                                message: "Pembayaran telah kami terima"
-                                            });
-                                        }
-                                    });
-                            }
-                        },
-                        {
-                            label: "Perpanjang",
-                            color: "white",
-                            handler: () => {
-                                store()
-                                    .dispatch("Payment/getPaymentUrl")
-                                    .then(res => {
-                                        cordova.InAppBrowser.open(
-                                            `${res.data.payment_url}`,
-                                            "_system",
-                                            "location=no"
-                                        );
-                                    });
-                            }
-                        }
-                    ]
-                });
+export default ({ app, router, Vue, store }) => {
+    axios.interceptors.response.use(
+        function(config) {
+            // Do something before request is sent
+            return config;
+        },
+        function(error) {
+            // Do something with request error
+            if (error.response.status == 401) {
+                store.commit("Auth/setUnauthorized", true);
+                store
+                    .dispatch("Auth/logout")
+                    .then(res => {
+                        Notify.create("Silahkan login kembali");
+                    });
+                // console.log(router().currentRoute);
             }
-        });
-}
+            return Promise.reject(error);
+        }
+    );
+
+    Vue.prototype.$axios = axios;
+    Vue.prototype.$http = axios;
+    const token = store.getters["Auth/token"];
+    if (token.access_token) {
+        Vue.prototype.$http.defaults.headers.common.Accept = "application/json";
+        Vue.prototype.$http.defaults.headers.common.Authorization = `${token.token_type} ${token.access_token}`;
+        // init
+        store
+            .dispatch("Auth/getAuth")
+            .then(res => {
+                if (
+                    res.data.user_activated_at != null &&
+                    moment(new Date()).diff(
+                        new Date(res.data.user_activated_at),
+                        "months",
+                        true
+                    ) > 6
+                ) {
+                    Notify.create({
+                        position: "center",
+                        message: "Masa Penggunaan aplikasi  RPP DIGITAL, PENILAIAN DIGITAL, dan MODUL DIGITAL sudah habis.  Silakan melakukan pembayaran iuran untuk menambah masa pemakaian.",
+                        textColor: "white",
+                        actions: [{
+                                label: "Konfirmasi",
+                                color: "white",
+                                handler: () => {
+                                    store
+                                        .dispatch("Auth/getPaymentStatus")
+                                        .then(res => {
+                                            if (
+                                                moment(new Date()).diff(
+                                                    new Date(res.data.user_activated_at),
+                                                    "months",
+                                                    true
+                                                ) <= 6
+                                            ) {
+                                                Notify.create({
+                                                    position: "center",
+                                                    message: "Pembayaran telah kami terima"
+                                                });
+                                            }
+                                        });
+                                }
+                            },
+                            {
+                                label: "Perpanjang",
+                                color: "white",
+                                handler: () => {
+                                    // store
+                                    //     .dispatch("Payment/getPaymentUrl")
+                                    //     .then(res => {
+                                    //         cordova.InAppBrowser.open(
+                                    //             `${res.data.payment_url}`,
+                                    //             "_system",
+                                    //             "location=no"
+                                    //         );
+                                    //     });
+                                    router.push('/payment')
+                                }
+                            }
+                        ]
+                    });
+                }
+            });
+    }
+};
--- src/pages/PaymentPage.vue
+++ src/pages/PaymentPage.vue
@@ -1,10 +1,73 @@
 <template>
-<div style="background-image:url('statics/images/payment.jpg');background-size:cover;height:100vh">
+  <div
+    style="
+      background-image: url('statics/images/payment.jpg');
+      background-size: cover;
+      height: 100vh;
+    "
+  >
     <div class="fixed-center text-center">
-        <q-card style="width:90vw">
-            <q-card-section>
+      <q-card style="width: 90vw">
+        <q-stepper v-model="step" vertical color="blue" animated>
+          <q-step
+            :name="1"
+            color="blue"
+            title="Pilih Metode Pembayaran"
+            icon="settings"
+            :done="step > 1"
+          >
+            <div class="q-pa-sm">
+              <div class="row">
+                <div class="col-12" v-if="payment_vendors.length">
+                  <q-btn
+                    color="blue"
+                    v-for="pv in payment_vendors"
+                    :key="pv.id"
+                    @click="() => step2(pv)"
+                    :loading="loading"
+                    :disable="loading"
+                    >Transfer ke Bank {{ pv.service_code }}</q-btn
+                  >
+                </div>
+                <div class="col-12" v-else>
+                  <q-spinner-ios color="blue" size="2em" />
+                </div>
+              </div>
+            </div>
+          </q-step>
+
+          <q-step
+            :name="2"
+            title="Pembayaran"
+            color="blue"
+            icon="create_new_folder"
+            :done="step > 2"
+          >
+            <div class="text-body1 text-left" v-if="payment_vendor">
+              Harap Melakukan Transfer ke
+              <b>BANK {{ payment_vendor.service_code.toUpperCase() }}</b> -
+              <b>{{ payment_vendor.account_number }}</b>
+            </div>
+            <div
+              class="text-body1 text-bold text-left q-pt-md q-pb-md"
+              v-if="payment_value"
+            >
+              Dengan Nominal Sebesar <br />Rp
+              {{ payment_value.toLocaleString() }}
+            </div>
+            <div class="text-body1 text-left q-pb-md" v-if="payment_vendor">
+              Harap transfer dengan <b>nominal yang sesuai</b> agar sistem dapat
+              melakukan pengecekan dengan tepat
+            </div>
+            <div class="text-body1 text-left">
+              Jika sudah melakukan pembayaran, silahkan tunggu
+              <b>5 menit</b> kemudian <b>Lakukan Pengecekan</b> melalui tombol
+              di bawah
+            </div>
+          </q-step>
+        </q-stepper>
+        <!-- <q-card-section>
                 <p>
-                    <!-- <img src="~assets/sad.svg" style="width:30vw;max-width:150px;" /> -->
                     <img src="statics/images/agpaii-logo-blue.png" style="width:30vw;max-width:150px;">
                 </p>
                 <q-btn flat color="blue" style="width:200px;" label="Lakukan pembayaran" @click="createTransaction" />
@@ -17,57 +80,125 @@
             </q-card-section>
             <q-card-actions class="row justify-end">
                 <q-btn color="blue" flat label="Keluar" @click="logout" />
-            </q-card-actions>
-        </q-card>
+            </q-card-actions> -->
+        <q-card-actions class="row justify-between">
+          <q-btn color="blue" flat label="Keluar" @click="logout" />
+          <q-btn
+            color="blue"
+            flat
+            label="Lakukan Pengecekan"
+            :loading="loading2"
+            :disable="loading2"
+            @click="check()"
+          ></q-btn>
+        </q-card-actions>
+      </q-card>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            loading: false
-        }
+  data() {
+    return {
+      loading: false,
+      loading2: false,
+      payment_value: null,
+      step: 1,
+      payment_vendors: [],
+      payment_vendor: null,
+    };
+  },
+  mounted() {
+    this.getPaymentVendors();
+  },
+  methods: {
+    getPaymentVendors() {
+      this.$store.dispatch("Payment/getPaymentVendors").then((res) => {
+        this.payment_vendors = res.data;
+      });
     },
-    methods: {
-        check() {
-            this.$store.dispatch("Auth/getPaymentStatus").then(res => {
-                //alert('test')
-
-                if (res.data.user_activated_at == null) {
-                    this.$q.notify("Terimakasih silahkan tunggu 1x24 jam");
-                } else {
-                    this.$store.dispatch("Auth/getAuth").then(res => {
-                        this.$q.notify("Pembayaran anda sudah kami terima");
-                        this.$router.push("/");
-                    });
-
-                }
+    step2(pv) {
+      this.loading = true;
+      this.getUniquePayment()
+        .then((res) => {
+          this.payment_vendor = pv;
+          this.step = 2;
+          this.loading = false;
+        })
+        .catch((err) => {
+          this.loading = false;
+        });
+    },
+    getUniquePayment() {
+      return new Promise((resolve, reject) => {
+        this.loading = true;
+        this.$store
+          .dispatch("Payment/getUniquePayment", { payment_vendor: 1 })
+          .then((res) => {
+            this.loading = false;
+            this.payment_value = res.data.value;
+            resolve(res);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    check() {
+      this.loading2 = true;
+      this.$store
+        .dispatch("Payment/confirmUniquePayment")
+        .then((res) => {
+          if (res.data.user_activated_at == null) {
+            this.loading2 = false;
+            this.$q.notify(
+              "Belum ada pembayaran yang kami terima. Tolong periksa apakah nominal yang anda kirim sudah sesuai"
+            );
+          } else {
+            this.$store.dispatch("Auth/getAuth").then((res) => {
+              this.loading2 = false;
+              this.$q.notify("Pembayaran anda sudah kami terima");
+              this.$router.push("/");
             });
-        },
-        logout() {
-            this.$store.dispatch("Auth/logout").then(() => {
-                this.$router.push("/login");
-            });
-        },
-        createTransaction() {
-            return new Promise((resolve, reject) => {
-                this.loading = true;
-                this.$store.dispatch("Payment/getPaymentUrl").then(res => {
-                    this.loading = false;
-                    if (this.$q.platform.is.mobile) {
-                        cordova.InAppBrowser.open(
-                            `${res.data.payment_url}`,
-                            "_system",
-                            "location=no"
-                        );
-                    } else {
-                        window.location.href = `${res.data.payment_url}`
-                    }
-                });
-            });
-        }
-    }
+          }
+        })
+        .catch((err) => {
+          console.log("error", err);
+          if (err.response.status == 404) {
+            this.$q.notify(
+              "Belum ada pembayaran yang kami terima. Tolong periksa apakah nominal yang anda kirim sudah sesuai"
+            );
+          } else if (err.response.data && err.response.data.message)
+            this.$q.notify(err.response.data.message);
+          else this.$q.notify(err.message);
+        })
+        .finally((_) => {
+          this.loading2 = false;
+        });
+    },
+    logout() {
+      this.$store.dispatch("Auth/logout").then(() => {
+        this.$router.push("/login");
+      });
+    },
+    createTransaction() {
+      return new Promise((resolve, reject) => {
+        this.loading = true;
+        this.$store.dispatch("Payment/getPaymentUrl").then((res) => {
+          this.loading = false;
+          if (this.$q.platform.is.mobile) {
+            cordova.InAppBrowser.open(
+              `${res.data.payment_url}`,
+              "_system",
+              "location=no"
+            );
+          } else {
+            window.location.href = `${res.data.payment_url}`;
+          }
+        });
+      });
+    },
+  },
 };
 </script>
