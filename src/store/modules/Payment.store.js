5fe2a6516862912364bba03ef69fd5fe7ffeaa89
unknown <rootrieqyns13@gmail.com>
2021-04-03 11:43:49 +0700

    back to midtrans

--- src/store/modules/Payment.store.js
+++ src/store/modules/Payment.store.js
@@ -1,68 +1,120 @@
 /* Module1.store.js */
-import axios from 'axios'
+import axios from "axios";
 // State object
-const state = {
-
-}
+const state = {};
 
 // Mutations
-const mutations = {
-
-}
+const mutations = {};
 
 // Actions
 const actions = {
+    getPaymentVendors({ commit }) {
+        return new Promise((resolve, reject) => {
+            axios
+                .get(`${this.state.Setting.url}/api/v1/paymentvendor`)
+                .then(res => {
+                    resolve(res);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
+    },
+    getUniquePayment({ commit }, payload) {
+        return new Promise((resolve, reject) => {
+            axios
+                .post(`${this.state.Setting.url}/api/v1/payments/getuniquepayment`, {
+                    ...payload
+                })
+                .then(res => {
+                    resolve(res);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
+    },
+    confirmUniquePayment() {
+        return new Promise((resolve, reject) => {
+            axios
+                .post(`${this.state.Setting.url}/api/v1/payments/confirmuniquepayment`)
+                .then(res => {
+                    resolve(res);
+                }).catch(err => {
+                    reject(err)
+                })
+        })
+    },
     getPaymentUrl({ commit }) {
         return new Promise((resolve, reject) => {
-            axios.post(`${this.state.Setting.url}/api/v1/payment/paymentUrl`).then((res) => {
-                resolve(res)
-            }).catch((err) => {
-                reject(err)
-            })
-        })
+            axios
+                .post(`${this.state.Setting.url}/api/v1/payment/paymentUrl`)
+                .then(res => {
+                    resolve(res);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
     },
     getDetailPayments({ commit }) {
         return new Promise((resolve, reject) => {
-            axios.get(`${this.state.Setting.url}/api/v1/payments/bymonthyear`).then((res) => {
-                resolve(res)
-            }).catch((err) => {
-                reject(err)
-            })
-        })
+            axios
+                .get(`${this.state.Setting.url}/api/v1/payments/bymonthyear`)
+                .then(res => {
+                    resolve(res);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
     },
-    getPaymentProvinces({commit},payload){
+    getPaymentProvinces({ commit }, payload) {
         return new Promise((resolve, reject) => {
-            axios.get(`${this.state.Setting.url}/api/v1/payments/bymonthyear/${payload.month}/${payload.year}`).then(res => {
-                resolve(res)
-            }).catch(err => {
-                reject(err)
-            })
-        })
+            axios
+                .get(
+                    `${this.state.Setting.url}/api/v1/payments/bymonthyear/${payload.month}/${payload.year}`
+                )
+                .then(res => {
+                    resolve(res);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
     },
-    getPaymentCities({commit},payload){
+    getPaymentCities({ commit }, payload) {
         return new Promise((resolve, reject) => {
-            axios.get(`${this.state.Setting.url}/api/v1/payments/bymonthyear/city/${payload.province_id}/${payload.month}/${payload.year}`).then(res => {
-                resolve(res)
-            }).catch(err => {
-                reject(err)
-            })
-        })
+            axios
+                .get(
+                    `${this.state.Setting.url}/api/v1/payments/bymonthyear/city/${payload.province_id}/${payload.month}/${payload.year}`
+                )
+                .then(res => {
+                    resolve(res);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
     },
-    getPaymentCityUsers({commit},payload){
+    getPaymentCityUsers({ commit }, payload) {
         return new Promise((resolve, reject) => {
-            axios.get(`${this.state.Setting.url}/api/v1/payments/bymonthyear/cityusers/${payload.city_id}/${payload.month}/${payload.year}`).then(res => {
-                resolve(res)
-            }).catch(err => {
-                reject(err)
-            })
-        })
+            axios
+                .get(
+                    `${this.state.Setting.url}/api/v1/payments/bymonthyear/cityusers/${payload.city_id}/${payload.month}/${payload.year}`
+                )
+                .then(res => {
+                    resolve(res);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
     }
-}
+};
 
 // Getter functions
-const getters = {
-
-}
+const getters = {};
 
 export default {
     namespaced: true,
@@ -70,4 +122,4 @@ export default {
     getters,
     actions,
     mutations
-}
+};
